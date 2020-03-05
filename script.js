68bad235e80c8bdd9d2b0bbc1195dac8831bd522
josehomero <jose.homero.rios@outlook.com>
2020-03-04 17:49:48 -0800

    updated

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const screen = document.getElementById('calculator-screen');
+
 
 const button = document.querySelector('button');
 
 const button1 = document.getElementById('number-button');
+
+function addText(e) {
+    const screen = document.getElementById('calculator-screen');
+    screen.textContent = e.target.value;
+}
 
 const calculatorButtons = document.querySelectorAll('.calculator-button');
 for(const buttons of calculatorButtons) {
